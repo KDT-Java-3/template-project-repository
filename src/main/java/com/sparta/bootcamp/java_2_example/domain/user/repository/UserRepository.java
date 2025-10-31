262947a9f07a63b1b85ef4b88d9a445325cb46f7
paullee714 <woolba714@kakao.com>
2025-10-31 15:14:19 +0900

    feat: week 01

---
+++ src/main/java/com/sparta/bootcamp/java_2_example/domain/user/repository/UserRepository.java
@@ -0,0 +1,27 @@
+package com.sparta.bootcamp.java_2_example.domain.user.repository;
+
+import com.sparta.bootcamp.java_2_example.domain.user.entity.User;
+import java.time.LocalDateTime;
+import java.util.List;
+import java.util.Optional;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+
+  Optional<User> findByEmail(String email);
+
+  List<User> findByCreatedAtAfterOrderByNameAsc(LocalDateTime dateTime);
+
+  long countByName(String name);
+
+  @Query("SELECT u FROM User u WHERE u.email = :email")
+  Optional<User> findUserByEmail(@Param("email") String email);
+
+  @Query("SELECT u FROM User u JOIN FETCH u.purchases")
+  List<User> findAllWithPurchases();
+
+}
