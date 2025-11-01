69a2279d2982c6200c67706da40faeabc1fb340f
pepponechoi <peppone.choi@gmail.com>
2025-11-01 13:16:30 +0900

    feat: 유저 컨트롤러 수정

--- src/main/java/com/pepponechoi/project/domain/user/controller/UserController.java
+++ src/main/java/com/pepponechoi/project/domain/user/controller/UserController.java
@@ -3,6 +3,7 @@ package com.pepponechoi.project.domain.user.controller;
 import com.pepponechoi.project.domain.user.dto.request.UserCreateRequest;
 import com.pepponechoi.project.domain.user.dto.response.UserResponse;
 import com.pepponechoi.project.domain.user.service.UserService;
+import java.net.URI;
 import java.util.List;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
@@ -12,14 +13,16 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController("api/users")
+@RestController("/api/users")
 @RequiredArgsConstructor
 public class UserController {
     private final UserService userService;
     @PostMapping
     public ResponseEntity<UserResponse> createUser(@RequestBody UserCreateRequest request) {
         UserResponse userResponse = userService.createUser(request);
-        return ResponseEntity.ok().body(userResponse);
+        return ResponseEntity
+            .created(URI.create(String.format("/api/users/%d", userResponse.id())))
+            .body(userResponse);
     }
 
     // 필터링은 나중에...
