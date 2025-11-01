413368fb836515e08eebaeeba4a421e6c1907998
one-zeze <tales0708@gmail.com>
2025-11-01 11:42:25 +0900

    init spring boot project

---
+++ src/main/java/com/sparta/proejct1101/Proejct1101Application.java
@@ -0,0 +1,13 @@
+package com.sparta.proejct1101;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Proejct1101Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Proejct1101Application.class, args);
+	}
+
+}
