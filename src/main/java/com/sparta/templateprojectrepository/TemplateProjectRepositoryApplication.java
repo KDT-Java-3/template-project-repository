cc66ba0df496bc78bbb732ed43f10593d2236b16
sooin92402 <sooin92402@gmail.com>
2025-11-01 21:14:03 +0900

    init

---
+++ src/main/java/com/sparta/templateprojectrepository/TemplateProjectRepositoryApplication.java
@@ -0,0 +1,16 @@
+package com.sparta.templateprojectrepository;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import java.io.FileDescriptor;
+
+@SpringBootApplication
+public class TemplateProjectRepositoryApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TemplateProjectRepositoryApplication.class, args);
+
+    }
+
+}
