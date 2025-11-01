8126de24a7c024e84145d5993ddf4e02a97f4c00
Robbie <Robbie@email.com>
2025-11-01 12:30:21 +0900

    chore: entity생성
    user, category, product, order, order_product, refund 엔티티 생성

---
+++ demo1/src/main/java/com/example/demo/domain/order/entity/Order.java
@@ -0,0 +1,58 @@
+package com.example.demo.domain.order.entity;
+
+import com.example.demo.domain.user.entity.User;
+import com.fasterxml.jackson.annotation.JsonBackReference;
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.experimental.FieldDefaults;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.DynamicInsert;
+import org.hibernate.annotations.DynamicUpdate;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+
+@Table
+@Entity
+@Getter
+@DynamicInsert
+@DynamicUpdate
+@NoArgsConstructor
+@FieldDefaults(level = AccessLevel.PRIVATE)
+public class Order {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    Long id;
+
+    @JsonBackReference
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id", nullable = false)
+    User user;
+
+    @Column(nullable = false)
+    BigDecimal totalPrice;
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false, length = 20)
+    OrderStatus status;
+
+    @Column(nullable = false, updatable = false)
+    @CreationTimestamp
+    LocalDateTime createdAt;
+
+    @Column(nullable = false)
+    @UpdateTimestamp
+    LocalDateTime updatedAt;
+
+    @Builder
+    public Order(User user, BigDecimal totalPrice, OrderStatus status){
+        this.user = user;
+        this.totalPrice = totalPrice;
+        this.status = status;
+    }
+}
---
+++ demo1/src/main/java/com/example/demo/domain/refund/entity/RefundStatus.java
@@ -0,0 +1,5 @@
+package com.example.demo.domain.refund.entity;
+
+public enum RefundStatus {
+    PENDING, COMPLETED, CANCELED
+}
