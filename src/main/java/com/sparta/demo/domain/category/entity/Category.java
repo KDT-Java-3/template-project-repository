0519c1f9e70f35b7331de3d4471d0a40e41147cc
alswosp0206 <alswosp0206@naver.com>
2025-11-01 17:15:17 +0900

    init: 기본 설정 + category/product

---
+++ src/main/java/com/sparta/demo/domain/category/entity/Category.java
@@ -0,0 +1,65 @@
+package com.sparta.demo.domain.category.entity;
+
+import com.fasterxml.jackson.annotation.JsonBackReference;
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.experimental.FieldDefaults;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.DynamicInsert;
+import org.hibernate.annotations.DynamicUpdate;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import java.time.LocalDateTime;
+
+@Table
+@Entity
+@Getter
+@DynamicInsert
+@DynamicUpdate
+@NoArgsConstructor
+@FieldDefaults(level = AccessLevel.PRIVATE)
+public class Category {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String name;
+
+    //@JsonBackReference
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "parent_id")
+    private Category parent;
+
+    @Column(nullable = false)
+    private String description;
+
+    @Column(nullable = false, updatable = false)
+    @CreationTimestamp
+    LocalDateTime createdAt;
+
+    @Column(nullable = false)
+    @UpdateTimestamp
+    LocalDateTime updatedAt;
+
+    @Builder
+    public Category(
+            String name,
+            Category parent,
+            String description
+    ) {
+        this.name = name;
+        this.parent = parent;
+        this.description = description;
+    }
+
+    public void update(String name, String description) {
+        this.name = name;
+        this.description = description;
+    }
+
+}
