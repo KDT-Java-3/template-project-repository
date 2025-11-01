77b8f5a97f777f2720757c34ad9c403f6cb7c9bb
bisksy <bizksy0731@gmail.com>
2025-11-02 02:01:58 +0900

    역할 분리

--- src/main/java/com/sparta/restful_1week/domain/refund/dto/RefundInDTO.java
+++ src/main/java/com/sparta/restful_1week/domain/refund/dto/RefundInDTO.java
@@ -1,12 +1,9 @@
 package com.sparta.restful_1week.domain.refund.dto;
 
-import jakarta.persistence.Column;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
-import org.hibernate.annotations.CreationTimestamp;
-import org.hibernate.annotations.UpdateTimestamp;
 
 import java.time.LocalDateTime;
 
