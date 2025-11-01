e28a6d319c6111d601298db33b17dfd97831bf37
bisksy <bizksy0731@gmail.com>
2025-11-01 10:55:18 +0900

    test

---
+++ src/main/java/com/sparta/restful_1week/Restful1weekApplication.java
@@ -0,0 +1,13 @@
+package com.sparta.restful_1week;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Restful1weekApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Restful1weekApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/sparta/restful_1week/Restful1weekApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sparta.restful_1week;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Restful1weekApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
