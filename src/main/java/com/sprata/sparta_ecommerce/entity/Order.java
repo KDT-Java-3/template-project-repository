3ed01b9ede4f6650ef16c26fd4c5dcb0a6ffadc9
jobmania <xzxz7003@naver.com>
2025-11-01 10:49:17 +0900

    <Entity> : 주문 관리 엔티티 추가.

---
+++ src/main/java/com/sprata/sparta_ecommerce/entity/Order.java
@@ -0,0 +1,42 @@
+package com.sprata.sparta_ecommerce.entity;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@Table(name = "orders") // 'order' is a reserved keyword in SQL
+public class Order extends Timestamped {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private Long userId;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "product_id", nullable = false)
+    private Product product;
+
+    @Column(nullable = false)
+    private int quantity;
+
+    @Column(nullable = false)
+    private String shippingAddress;
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    private OrderStatus status;
+
+    public Order(Long userId, Product product, int quantity, String shippingAddress) {
+        this.userId = userId;
+        this.product = product;
+        this.quantity = quantity;
+        this.shippingAddress = shippingAddress;
+        this.status = OrderStatus.PENDING;
+    }
+}
