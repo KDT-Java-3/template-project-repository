047aa546cdac59275d7374ee1d36160f46dc35ba
ysh <woslq0019@gmail.com>
2025-11-01 10:50:51 +0900

    init

---
+++ src/main/java/com/sparta/week01project/Week01ProjectApplication.java
@@ -0,0 +1,13 @@
+package com.sparta.week01project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Week01ProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Week01ProjectApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/sparta/week01project/Week01ProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sparta.week01project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Week01ProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
