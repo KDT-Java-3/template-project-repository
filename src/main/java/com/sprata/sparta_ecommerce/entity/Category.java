7b0fb08c72173f15e570a5a7a3428c8ca8a3bb42
jobmania <xzxz7003@naver.com>
2025-11-01 10:49:46 +0900

    <Entity> : 카테고리 엔티티 추가

---
+++ src/main/java/com/sprata/sparta_ecommerce/entity/Category.java
@@ -0,0 +1,28 @@
+package com.sprata.sparta_ecommerce.entity;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@Table(name = "category")
+public class Category extends Timestamped {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, unique = true)
+    private String name;
+
+    @Column(nullable = false)
+    private String description;
+
+    public Category(String name, String description) {
+        this.name = name;
+        this.description = description;
+    }
+}
