6ffda90a14076d1d8e8ef5e26cdae334051c56bb
jaehyeok <jaehyuk.dev@gmail.com>
2025-11-02 01:14:44 -0700

    init project

---
+++ src/test/java/com/jaehyuk/week_01_project/Week01ProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jaehyuk.week_01_project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Week01ProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
