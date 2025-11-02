88713983fdf89769132b9b82d6d39c607a652331
HwajungChoi <fa4432@naver.com>
2025-11-02 21:41:36 +0900

    프로젝트명 변경

--- src/main/java/com/sparta/commerce/CommerceApplication.java
+++ src/main/java/com/sparta/commerce/CommerceApplication.java
@@ -1,13 +1,13 @@
-package com.sparta.sparta;
+package com.sparta.commerce;
 
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class SpartaApplication {
+public class CommerceApplication {
     public static void main(String[] args) {
-        SpringApplication.run(SpartaApplication.class, args);
+        SpringApplication.run(CommerceApplication.class, args);
     }
 }
 
