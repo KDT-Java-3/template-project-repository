8887f84e30918998659de176952cd4f589cd5fb7
sangminna <sm9171@nate.com>
2025-11-02 16:03:35 +0900

    애플리케이션 설정 및 메인 클래스 추가
    - Spring Boot 메인 애플리케이션 클래스
    - application.yml: H2 데이터베이스 설정, JPA 설정, Flyway 마이그레이션 활성화
    - 테스트 클래스 기본 구조

---
+++ sangmin/src/test/java/com/sparta/sangmin/SangminApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sparta.sangmin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SangminApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
