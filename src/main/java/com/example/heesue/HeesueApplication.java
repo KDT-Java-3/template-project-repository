9382ff93637a657b453e1f55b6533c1e26f59877
heeppyea <phs8743@naver.com>
2025-11-02 17:29:14 +0900

    create: 프로젝트 생성

---
+++ src/main/java/com/example/heesue/HeesueApplication.java
@@ -0,0 +1,13 @@
+package com.example.heesue;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HeesueApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HeesueApplication.class, args);
+    }
+
+}
