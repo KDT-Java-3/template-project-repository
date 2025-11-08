6f9c0aa317d0c5bd4989bc590ad679b995037c14
waba82 <vkfqkddl123@naver.com>
2025-11-08 17:59:25 +0900

    수정 ProductRepository.java - 이름으로 존재 여부 조회 메서드 추가

--- jae-commerce/src/main/java/com/sparta/jc/domain/product/repository/ProductRepository.java
+++ jae-commerce/src/main/java/com/sparta/jc/domain/product/repository/ProductRepository.java
@@ -2,7 +2,6 @@ package com.sparta.jc.domain.product.repository;
 
 import com.sparta.jc.domain.product.entity.Product;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 import org.springframework.stereotype.Repository;
 
 /**
@@ -10,5 +9,10 @@ import org.springframework.stereotype.Repository;
  * JpaSpecificationExecutor → 검색/필터링 조건을 조합할 수 있도록 지원
  */
 @Repository
-public interface ProductRepository extends JpaRepository<Product, Long>, JpaSpecificationExecutor<Product> {
+public interface ProductRepository extends JpaRepository<Product, Long> {
+
+    /**
+     * 여부 by
+     */
+    boolean existsByName(String name);
 }
