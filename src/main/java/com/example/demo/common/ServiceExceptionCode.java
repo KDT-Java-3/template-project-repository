9927899fa42c0ec88b07e63485a13abce7485e0f
paullee714 <woolba714@kakao.com>
2025-11-11 18:55:59 +0900

    Update ServiceExceptionCode.java

--- src/main/java/com/example/demo/common/ServiceExceptionCode.java
+++ src/main/java/com/example/demo/common/ServiceExceptionCode.java
@@ -7,6 +7,7 @@ import lombok.Getter;
 @Getter
 @AllArgsConstructor
 public enum ServiceExceptionCode {
+    INTERNAL_ERROR("서버에러"),
     // 서비스 로직에서 필요한 Exception들을 Enum
     NOT_FOUND_PRODUCT("상품을 찾을수 없습니다"),
     NOT_FOUND_USER("사용자를 찾을 수 없습니다"),
